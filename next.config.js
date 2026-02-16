f800c2a66251c04b90dd35df99c86ee88733fe53
SarveshAngadi9 <sarveshangadi9@gmail.com>
2026-02-16 17:03:08 +0530

    ignore

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export',           // static HTML export
+  basePath: '/Portfolio',     // GitHub repo name
+  assetPrefix: './',          // use relative paths for _next
+  trailingSlash: true,        // ensures proper folder structure
+}
+
+module.exports = nextConfig
